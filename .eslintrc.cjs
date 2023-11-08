5066fa040932d85ace95df19838a51153ec5e677
KimMyeongSu <Myeongsu@likelion.org>
2023-11-08 09:12:34 +0000

    ESLint , 프리티어 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  evn: {
+    'vue/setup-compiler-macros': true
+  },
+  rules: {
+    // 'no-console': true // 콘솔 사용 x 설정값
+    //이런식으로 운영에선 사용하지않도록 설정도 가능
+    'no-console': process.evn.NODE_ENV === 'production' ? 'error' : 'off'
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
+    // alias 파일 시스템 경로에 대한 별칭
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
